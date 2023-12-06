8d944c52eaf19a00146f68c8de0ebeac3b8f6d0f
MathiusMendoza <mendozamathius9@gmail.com>
2023-12-06 16:23:40 -0500

    oki

--- ManejoCicloFor/src/paquete01/Ejemplo07.java
+++ ManejoCicloFor/src/paquete01/Ejemplo07.java
@@ -13,15 +13,22 @@ public class Ejemplo07 {
     public static void main(String[] args) {
         int operacion;
         String mensajeFinal = "";
-        for (int i = 1; i <= 10; i++) { // for 01
+        // Se puede hacer un ciclo for dentro de otro ciclo for.
+        for (int i = 1; i <= 50; i++) { // for 01
             // System.out.printf("Tabla de multiplicar del número %d\n", i);
             mensajeFinal = String.format("%sTabla de multiplicar del número %d\n", 
                     mensajeFinal, i);
             for (int contador = 1; contador <= 12; contador++) {
                 operacion = i * contador;
-                // System.out.printf("%d x %d = %d\n", i, contador, operacion);
+                //=System.out.printf("%d x %d = %d\n", i, contador, operacion);
+                // Con es siguiente cambio se presentan las tablas de multiplicar.
+                mensajeFinal = String.format("%s%d x %d = %d\n", mensajeFinal,
+                        i, contador, operacion);
+                
             }
             // System.out.println("\n");
+            // La siguiente linea de codigo presenta un salto de linea al terminar la tabla.
+            mensajeFinal = String.format("%s\n",mensajeFinal);
         } // termina for 01
         
         System.out.printf("%s\n", mensajeFinal);
